346cdeb96fca0ba109df6138073c5140fc7252f5
Peter Drews <petermdrews@gmail.com>
2021-05-12 16:22:38 -0500

    Added Campsite Info Component

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import Directory from "./DirectoryComponent";
+import CampsiteInfo from "./CampsiteInfoComponent";
+import { View } from "react-native";
 import { CAMPSITES } from "../shared/campsites";
 
 class Main extends Component {
@@ -7,11 +9,33 @@ class Main extends Component {
     super(props);
     this.state = {
       campsites: CAMPSITES,
+      selectedCampsite: null,
     };
   }
 
+  //Event Handler - updates the state based on the which is selected
+  onCampsiteSelect(campsiteId) {
+    this.setState({ selectedCampsite: campsiteId });
+  }
+
   render() {
-    return <Directory campsites={this.state.campsites} />;
+    return (
+      <View style={{ flex: 1 }}>
+        <Directory
+          campsites={this.state.campsites}
+          onPress={(campsiteId) => this.onCampsiteSelect(campsiteId)}
+        />
+        {/*The onCampsiteSelect method above is stored in the Directory component
+        so it is available to be called there when needed*/}
+        <CampsiteInfo
+          campsite={
+            this.state.campsites.filter(
+              (campsite) => campsite.id === this.state.selectedCampsite
+            )[0]
+          }
+        />
+      </View>
+    );
   }
 }
 
