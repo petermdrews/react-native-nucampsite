f17572a4a877635ffc206a25b6efb52f14de972f
Peter Drews <petermdrews@gmail.com>
2021-05-13 14:51:52 -0500

    Added First Navigational Elements

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { Component } from "react";
 import { Text, View } from "react-native";
 import { Card } from "react-native-elements";
+import { CAMPSITES } from "../shared/campsites";
 
 function RenderCampsite({ campsite }) {
   if (campsite) {
@@ -16,8 +17,34 @@ function RenderCampsite({ campsite }) {
   return <View />;
 }
 
-function CampsiteInfo(props) {
-  return <RenderCampsite campsite={props.campsite} />;
+class CampsiteInfo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      campsites: CAMPSITES,
+    };
+  }
+
+  //Code below sets the header for the screen
+  static navigationOptions = {
+    title: "Campsite Information",
+  };
+
+  render() {
+    /*The line of code below change how the campites object is passed
+    to the RenderCampsite component. In the Directory Component, we set a parameter
+    to hold the campsiteId being passed with the navigate function. It can be accessed here
+    through the navigation prop, which is passed automatically to all components that
+    are set up as screens. */
+    const campsiteId = this.props.navigation.getParam("campsiteId");
+    /* Because the campsite array is stored in the local state, 
+    the line of code below pulls the campsite object we want from 
+    the array using the filter method */
+    const campsite = this.state.campsites.filter(
+      (campsite) => campsite.id === campsiteId
+    )[0];
+    return <RenderCampsite campsite={campsite} />;
+  }
 }
 
 export default CampsiteInfo;
--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,25 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 import { FlatList } from "react-native";
 import { ListItem } from "react-native-elements";
+import { CAMPSITES } from "../shared/campsites";
 
-function Directory(props) {
-  const renderDirectoryItem = ({ item }) => {
-    return (
-      <ListItem
-        title={item.name}
-        subtitle={item.description}
-        onPress={() => props.onPress(item.id)}
-        leftAvatar={{ source: require("./images/react-lake.jpg") }}
-      /> /* The require function above is built in as part of node.js*/
-    );
+class Directory extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      campsites: CAMPSITES,
+    };
+  }
+
+  static navigationOptions = {
+    title: "Directory",
   };
-  return (
-    <FlatList
-      data={props.campsites}
-      renderItem={renderDirectoryItem}
-      keyExtractor={(item) => item.id.toString()}
-    />
-  );
+
+  render() {
+    //The line below is destructuring the navigate function out of the navigation prop
+    const { navigate } = this.props.navigation;
+    const renderDirectoryItem = ({ item }) => {
+      return (
+        <ListItem
+          title={item.name}
+          subtitle={item.description}
+          onPress={() =>
+            navigate("CampsiteInfo", { campsiteId: item.id })
+          } /*this uses the navigate function
+          to navigate on press. The first argument is the screen to navigate to. The second optional argument adds
+          optional parameters to the route. In this case, we're specifying a parameter called campsiteId
+          and giving the id of the campsite that was pressed  */
+          leftAvatar={{ source: require("./images/react-lake.jpg") }}
+        /> // The require function above is built in as part of node.js
+      );
+    };
+    return (
+      <FlatList
+        data={this.state.campsites} //the campsites.js data is stored in this compnents state; it isn't being received as props
+        renderItem={renderDirectoryItem}
+        keyExtractor={(item) => item.id.toString()}
+      />
+    );
+  }
 }
 
 export default Directory;
