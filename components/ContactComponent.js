1f1ef76d5287c003e8bda959d74867ad18d1ba14
Peter Drews <petermdrews@gmail.com>
2021-05-15 12:20:21 -0500

    End of Workshop 1

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import { ScrollView } from "react-native";
+import { ScrollView, Text } from "react-native";
 import { ListItem } from "react-native-elements";
 import { CAMPSITES } from "../shared/campsites";
+import { Card } from "react-native-elements";
 
 class Contact extends Component {
   constructor(props) {
@@ -13,7 +14,16 @@ class Contact extends Component {
   };
 
   render() {
-    return <ScrollView></ScrollView>;
+    return (
+      <ScrollView>
+        <Card title="Contact Information" wrapperStyle={{ margin: 20 }}>
+          <Text>1 Nucamp Way</Text>
+          <Text style={{ marginBottom: 10 }}>Seattle, WA 98001 U.S.A.</Text>
+          <Text>Phone: 1-206-555-1234</Text>
+          <Text>Email: campsites@nucamp.co</Text>
+        </Card>
+      </ScrollView>
+    );
   }
 }
 
